2892667a1d1f8f07247b1872f569f2c042d8f569
Tiago Dexheimer <tiagodexheimer@gmail.com>
2025-08-02 09:47:24 -0300

    fix package

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { AuthProvider } from '@/contexts/AuthContext';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { MapProvider } from '@/contexts/MapContext';
